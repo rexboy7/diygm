c2797ee32a29609cd61192886c39283f766d35c7
Rex Li <rexli@linecorp.com>
2021-05-16 01:20:42 +0900

    add activity page and dummy data

---
+++ src/data/activitiesData.js
@@ -0,0 +1,32 @@
+import activities1 from "../assets/activities1.jpg";
+import activities2 from "../assets/activities2.jpg";
+import activities3 from "../assets/activities3.jpg";
+export const activitiesData = [
+  {
+    id: 1,
+    title: "人生畫廊",
+    image: activities1,
+    groupName: "柒伍壹遊戲",
+    description: `​我們是「柒伍壹遊戲」，​
+    立志做怪怪的遊戲活在這個社會上。`,
+    time: "11:00 - 12:00",
+  },
+  {
+    id: 2,
+    title: "守夜人：長夜",
+    image: activities2,
+    groupName: "玻璃心工作室",
+    description: `​一個 HARDCORE 的工作室，
+    ​一心集結所有構成好遊戲的要素。`,
+    time: "14:00 - 15:00",
+  },
+  {
+    id: 3,
+    title: "​炎姬",
+    image: activities3,
+    groupName: "夕暮工作室",
+    description: `​​​目前正在獨立開發中，
+    ​希望未來將會與大家碰面。`,
+    time: "16:00 - 17:00",
+  },
+];
